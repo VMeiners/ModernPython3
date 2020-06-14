ba7e88727803cf27571e6a05722152fdb042b8d9
VMeiners <64986104+VMeiners@users.noreply.github.com>
2020-06-13 19:43:40 -0600

    Add files via upload

---
+++ RPSv3.py
@@ -0,0 +1,35 @@
+import random
+
+print("Rock...")
+print("Paper...")
+print("Scissors...")
+
+player = input("Player, make your move: ").lower()
+rand_num = random.randint(0,2)
+if rand_num == 0:
+	computer = "rock"
+elif rand_num == 1:
+	computer = "paper"
+else:
+	computer = "scissors"
+print(f"Computer plays {computer}")
+
+if player == computer:
+	print("It's a tie!")
+elif player == "rock":
+	if computer == "scissors":
+		print("Player wins!")
+	elif computer == "paper":
+		print("Computer wins!")
+elif player == "paper":
+		if computer == "rock":
+			print("Player wins!")
+		elif computer == "scissors":
+			print("Computer wins!")
+elif player == "scissors":
+		if computer == "rock":
+			print("Computer wins!")
+		elif computer == "paper":
+			print("Player wins!")
+else:
+	print("Please enter a valid move.")
